64771957e4b9f2f427efa26eba5a5d6f45588bc2
kadour22 <zaroui.abedlkader777@gmail.com>
2026-05-21 10:57:45 -0400

    add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navigation/Navbar'
+import CreateIncidentRaport from './components/Incidents/CreateIncidentRaport'
+import Login from './components/Users/Login'
 const App = () => {
   return (
-    <div>
-      <h1>Home page</h1>
-    </div>
+    <BrowserRouter>
+      <Navbar/>
+      <Routes>
+         <Route path='/create-incident/' element={<CreateIncidentRaport/>} />
+         <Route path='/login/' element={<Login/>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
